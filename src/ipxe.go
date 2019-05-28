fa90f0ac2b083d01abd18d7ee80eb3137821be67
Dan Prince <dprince@redhat.com>
2019-05-28 13:34:50 -0400

    Formatting. And sanboot instead of exit.

--- src/ipxe.go
+++ src/ipxe.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "net/http"
-    "os"
-    "strings"
-    "time"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    addr_file := strings.Split(r.RemoteAddr, ":")[0]
-    file, err := os.Open(addr_file)
-    if err != nil {
-        fmt.Fprintln(w, "#!ipxe")
-        fmt.Fprintln(w, "exit")
-    } else {
-        fmt.Printf("%s Serving: %s\n", time.Now().Format("2006-01-02 15:04:05"), addr_file);
-        io.Copy(w, file)
-        os.Remove(addr_file)
-    }
+	addr_file := strings.Split(r.RemoteAddr, ":")[0]
+	file, err := os.Open(addr_file)
+	if err != nil {
+		fmt.Fprintln(w, "#!ipxe")
+		fmt.Fprintln(w, "echo no custom ipxe script found. booting locally...")
+		//fmt.Fprintln(w, "exit")
+		fmt.Fprintln(w, "sanboot --no-describe --drive 0x80")
+	} else {
+		fmt.Printf("%s Serving: %s\n", time.Now().Format("2006-01-02 15:04:05"), addr_file)
+		io.Copy(w, file)
+		os.Remove(addr_file)
+	}
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":10080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":10080", nil)
 }
